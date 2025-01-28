6757b83e68a4cc68e544ff83dcfdf5473e633d77
Anwarr Turay <anwarrturay@icloud.com>
2025-01-28 00:58:14 -0800

    worked on the server

--- server/server.js
+++ server/server.js
@@ -33,25 +33,31 @@ app.post('/', (req, res) => {
 });
 
 // API route for signup
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { first_name, last_name, email, id_number, password, department, level, image } = req.body;
 
-  // const encryptedPassword = bcrypt.hash(password, 10);
+  try{
 
-  // Validate all required fields
-  if (!first_name || !last_name || !email || !id_number || !password || !department || !level || !image) {
-    return res.status(400).send({ message: 'All fields are required' });
-  }
+    const encryptedPassword = await bcrypt.hash(password, 10);
 
-  const query = `INSERT INTO registration (first_name, last_name, email, id_number, password, department, level, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-  dbConn.query(query, [first_name, last_name, email, id_number, password, department, level, image], (err, results) => {
-    if (err) {
-      console.error('Error during signup:', err);
-      res.status(500).send({ message: 'Internal server error' });
-    } else {
-      res.status(201).send({ message: `A user with name: ${first_name} ${last_name} has registered` });
+    // Validate all required fields
+    if (!first_name || !last_name || !email || !id_number || !password || !department || !level || !image) {
+      return res.status(400).send({ message: 'All fields are required' });
     }
-  });
+
+    const query = `INSERT INTO registration (first_name, last_name, email, id_number, password, department, level, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    dbConn.query(query, [first_name, last_name, email, id_number, encryptedPassword, department, level, image], (err, results) => {
+      if (err) {
+        console.error('Error during signup:', err);
+        res.status(500).send({ message: 'Internal server error' });
+      } else {
+        res.status(201).send({ message: `A user with name: ${first_name} ${last_name} has registered` });
+      }
+    });
+  }catch(err){
+    console.error(err);
+    res.status(500).json({message: "Server error"})
+  }
 });
 
 // Start the server
