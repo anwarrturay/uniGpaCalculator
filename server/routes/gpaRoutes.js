b3b16817f715de52eac159b6031a3826de71ac28
Julius Khamara <khamarajulius@gmail.com>
2025-02-04 03:04:48 +0000

    Wahala

--- server/routes/gpaRoutes.js
+++ server/routes/gpaRoutes.js
@@ -1,12 +1,4 @@
 import express from 'express';
-
-import {calculateGPA} from '../controller/gpaController.js';
-import { saveCalculation } from '../controller/gpaController.js';
-const router = express.Router();
-
-router.post('/calculate', calculateGPA);
-router.post('/save', saveCalculation);
-
 import { calculateGPA, saveGPA, getCGPA } from '../controller/gpaController.js';
 const router = express.Router();
 
