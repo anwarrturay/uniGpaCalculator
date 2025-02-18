716fa7e7b9ecdec14c4eb7a4850f1ab39f981702
Anwarr Turay <anwarrturay@icloud.com>
2025-02-18 00:58:37 -0800

    fixed the performance issues

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -4,7 +4,7 @@ const Registration = require("../models/Registration")
 const getSpecificUser = async (req, res) =>{
     if(!req?.params?.id) return res.status(400).json({message: "No user found"});
     
-    const user = await Registration.findOne({_id: req.params.id});
+    const user = await Registration.findOne({_id: req.params.id}).exec();
     if(!user){
         return res.status(204).json({message: `No User with ID: ${req.params.id} found.`});
     }
